8ea310ccd1e17f47ebb33166ab4d9fbde5477270
Daniel Harju <1975danhar@gmail.com>
2013-08-04 14:14:28 +0200

    ....

---
+++ buGPS.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include"readSerial.h"
+#include"list.h"
+#include"defines.h"
+#include"parseNMEA.h"
+static char *getGpgga(char *retval, int i)
+{
+	GPGGA *gpgga;
+	char line[100];
+	
+	gpgga = malloc(sizeof(GPGGA));
+	pthread_mutex_lock(&GPGGAMutex);
+	strcpy(line, GPGGA_LIST->string);
+	pthread_mutex_unlock(&GPGGAMutex);
+	parseNMEA(gpgga, line);
+	switch (i){
+		case 0:
+			strcpy(retval, gpgga->MessageID);
+			break;
+		case 1:
+			strcpy(retval, gpgga->UTCTime);
+			break;
+		case 2:
+			strcpy(retval, gpgga->Latitude);
+			break;
+		case 3:
+			strcpy(retval, gpgga->NSIndicator);
+			break;
+		case 4:
+			strcpy(retval, gpgga->Longitude);
+			break;
+		case 5:
+			strcpy(retval, gpgga->EWIndicator);
+			break;
+		case 6:
+			strcpy(retval, gpgga->PositionFixIndicator);
+			break;
+		case 7:
+			strcpy(retval, gpgga->SatellitesUsed);
+			break;
+		case 8:
+			strcpy(retval, gpgga->HDOP);
+			break;
+		case 9:
+			strcpy(retval, gpgga->MSLAltitude);
+			break;
+		case 10:
+			strcpy(retval, gpgga->MSLUnits);
+			break;
+		case 11:
+			strcpy(retval, gpgga->GeoidSeparation);
+			break;
+		case 12:
+			strcpy(retval, gpgga->GeoidUnits);
+			break;
+		case 13:
+			strcpy(retval, gpgga->AgeOfDiffCorr);
+			break;
+		case 14:
+			strcpy(retval, gpgga->DiffRefStationID);
+			break;
+		case 15:
+			strcpy(retval, gpgga->Checksum);
+			break;
+	}
+	free(gpgga);
+	return retval;
+}
+char *getMessageID(char *retval){return getGpgga(retval, 0);}
+char *getUTCTime(char *retval){return getGpgga(retval, 1);}
+char *getLatitude(char *retval){return getGpgga(retval, 2);}
+char *getNSIndicator(char *retval){return getGpgga(retval, 3);}
+char *getLongitude(char *retval){return getGpgga(retval, 4);}
+char *getEWIndicator(char *retval){return getGpgga(retval, 5);}
+char *getPositionFixIndicator(char *retval){return getGpgga(retval, 6);}
+char *getSatellitesUsed(char *retval){return getGpgga(retval, 7);}
+char *getHDOP(char *retval){return getGpgga(retval, 8);}
+char *getMSLAltitude(char *retval){return getGpgga(retval, 9);}
+char *getMSLUnits(char *retval){return getGpgga(retval, 10);}
+char *getGeoidSeparation(char *retval){return getGpgga(retval, 11);}
+char *getGeoidUnits(char *retval){return getGpgga(retval, 12);}
+char *getAgeOfDiffCorr(char *retval){return getGpgga(retval, 13);}
+char *getDiffRefStation(char *retval){return getGpgga(retval, 14);}
+char *getChecksum(char *retval){return getGpgga(retval, 15);}
+
+int BUinitGPS(int flags, int numberOfObjects)
+{
+	int pt;
+	pthread_mutex_init(&GPGSAMutex, NULL);
+	pthread_mutex_init(&GPRMCMutex, NULL);
+	pthread_mutex_init(&GPGGAMutex, NULL);
+	pthread_mutex_init(&GPGSVMutex, NULL);
+
+	pthread_t thread1;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pt = pthread_create(&thread1, &attr, readSerial, NULL);
+	if (pt){
+		printf("Error could not create thread!\n");
+		exit(-1);
+	}
+	return pt;
+}
---
+++ buGPS.h
@@ -0,0 +1,21 @@
+#ifndef BUGPS_H
+#define BUGPS_H
+
+int BUinitGPS(int flags, int numberOfObjects);
+char *getMessageID(char *retval); 
+char *getUTCTime(char *retval);
+char *getLatitude(char *retval);
+char *getNSIndicator(char *retval);
+char *getLongitude(char *retval);
+char *getEWIndicator(char *retval);
+char *getPositionFixIndicator(char *retval);
+char *getSatellitesUsed(char *retval);
+char *getHDOP(char *retval);
+char *getMSLAltitude(char *retval);
+char *getMSLUnits(char *retval);
+char *getGeoidSeparation(char *retval);
+char *getGeoidUnits(char *retval);
+char *getAgeOfDiffCorr(char *retval);
+char *getDiffRefStation(char *retval);
+char *getChecksum(char *retval);
+#endif
